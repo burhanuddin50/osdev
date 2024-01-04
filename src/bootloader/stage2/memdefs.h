65b6170c40023435b8be3b3cdc4f170288a689d8
Burhanuddin <f20212905@hyderabad.bits-pilani.ac.in>
2024-01-04 13:39:33 +0530

    Part 6

---
+++ src/bootloader/stage2/memdefs.h
@@ -0,0 +1,6 @@
+#pragma once
+#define MEMORY_MIN 0x00000500
+#define MEMORY_MAX 0x00080000
+
+#define MEMORY_FAT_ADDR     ((void far*)0x00500000)         // segment:offset (SSSSOOOO)
+#define MEMORY_FAT_SIZE     0x00010000
